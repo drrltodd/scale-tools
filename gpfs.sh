bcb41128109f0599f9862a16be853ecf0fdd8da0
R. Lindsay Todd <lindsay@oc0571364225.ibm.com>
2016-11-22 18:21:42 -0500

    Scriptlet for profile

---
+++ gpfs.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+for pathVar in /usr/lpp/mmfs/bin /opt/ibm/MCStore/bin; do
+    if [ ! -d $pathVar ]; then
+         continue
+    fi
+
+    case $PATH in
+    *:$pathVar)
+	;;
+    *:$pathVar:*)
+	;;
+    $pathVar:*)
+	;;
+    *)
+	PATH=$pathVar:$PATH
+	;;
+    esac
+done
+
